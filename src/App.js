4c850459cd55c66872e6377e0f63ca974cbc63d8
Kevin Binu Thottumkal <kevin.binu-thottumkal@mohawkcollege.ca>
2024-02-20 17:00:13 -0500

    Routing updated and firebase package installed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import Home from "./components/routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
+
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 // import Directory from './components/directory/directory.component';
 
@@ -8,7 +11,11 @@ import { Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />}/>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} /> 
+        <Route path="/shop" />
+        <Route path="sign-in" element={<SignIn />} />
+      </Route>
     </Routes>
   );
 };
