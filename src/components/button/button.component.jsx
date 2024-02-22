3c4ea31985db2d66a00340bd5b4b9c3b34ea038a
Kevin Binu Thottumkal <kevin.binu-thottumkal@mohawkcollege.ca>
2024-02-22 02:45:42 -0500

    added and styles button component for the sign-up form.

---
+++ src/components/button/button.component.jsx
@@ -0,0 +1,25 @@
+/* 
+    default
+    inverted
+    google sign in
+*/
+
+import './button.styles.scss';
+
+const BUTTON_TYPE_CLASSES = {
+    inverted: 'inverted',
+    google: 'google-sign-in',
+}
+
+const Button = ({children, buttonType, ...otherProps}) => {
+    return (
+        <button 
+        className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
+        {...otherProps}
+        >
+            {children}
+        </button>
+    )
+}
+
+export default Button;
