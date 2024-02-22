8ee034d65066f177b76ac2ab0d9439c358c80fc7
Kevin Binu Thottumkal <kevin.binu-thottumkal@mohawkcollege.ca>
2024-02-22 03:21:16 -0500

    sign-in component created and the authentication route page updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
-import SignIn from "./routes/sign-in/sign-in.component";
+import Authentication from "./routes/authentication/authentication.component";
 
 // import Directory from './components/directory/directory.component';
 
@@ -14,7 +14,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} /> 
         <Route path="/shop" />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -8,8 +8,11 @@ import {
 } from "../../utils/firebase/firebase.utils";
 
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
-const SignIn = () => {
+import './authentication.styles.scss';
+
+const Authentication = () => {
     useEffect(async () => {
         const response = await getRedirectResult(auth);
         // console.log(response);
@@ -17,11 +20,6 @@ const SignIn = () => {
             const userDocRef = await createUserDocumentFromAuth(response.user);
         }
     }, [])
-
-    const logGoogleUser = async () => {
-        const { user } = await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user);
-    }
 
     // Will not console.log because we are redirected to a different domain. 
     // So it will not process the code after the redirect
@@ -32,13 +30,13 @@ const SignIn = () => {
     // }
 
     return (
-        <div>
-            <h1>Sign In</h1>
-            <button onClick={logGoogleUser}>Sign In with Google Popup</button>
-            <button onClick={signInWithGoogleRedirect}>Sign In with Google Redirect</button>
+        <div className="authentication-container">
+            {/* <button onClick={logGoogleUser}>Sign In with Google Popup</button>
+            <button onClick={signInWithGoogleRedirect}>Sign In with Google Redirect</button> */}
+            <SignInForm />
             <SignUpForm />
         </div>
     )
 }
 
-export default SignIn;
+export default Authentication;
