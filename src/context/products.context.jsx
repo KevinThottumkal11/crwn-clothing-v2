b2852f92b049f9f9d161ce4d5ba7e503c720d256
Kevin Binu Thottumkal <kevinthottumkal11@gmail.com>
2024-02-28 04:34:14 -0500

    add data to firebase and added functionality to retrieve the data in a hashmap instead of in an array.

--- src/context/products.context.jsx
+++ src/context/products.context.jsx
@@ -1,14 +1,29 @@
 import { createContext, useState, useEffect } from 'react';
 
-import PRODUCTS from '../shop-data.json'
+// import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils.js';
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils.js';
+// import SHOP_DATA from '../shop-data.js'
 
 export const ProductsContext = createContext({
     products: [],
 })
 
 export const ProductProvider = ({ children }) => {
-    const [products, setProducts] = useState(PRODUCTS);
-    const value = { products };
+    const [products, setProducts] = useState([]);
+    
+    // Only for adding data into the firebase db, Do not need to use once the data is loaded
+    // useEffect(() => {
+    //     addCollectionAndDocuments('categories', SHOP_DATA);
+    // }, [])
+
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments();
+            console.log(categoryMap);
+        }
+        getCategoriesMap();
+    }, [])
+    const value = { products }; 
 
     return (
         <ProductsContext.Provider value={value}>{ children }</ProductsContext.Provider>
