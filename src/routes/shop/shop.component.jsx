a2ea0057a2ed37422fb96bb16118280f35c31b1b
Kevin Binu Thottumkal <kevinthottumkal11@gmail.com>
2024-02-28 17:36:23 -0500

    creating Routes in shop component using category component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,24 +1,35 @@
+import { Routes, Route } from "react-router-dom";
+
 // import SHOP_DATA from '../../shop-data.json';
-import { Fragment, useContext } from "react";
+// import { Fragment, useContext } from "react";
 
-import { CategoriesContext } from "../../context/categories.context";
+// import { CategoriesContext } from "../../context/categories.context";
 
-import ProductCard from "../../components/product-card/product-card.component";
-import CategoryPreview from "../../components/category-preview/category-preview.component";
+// import ProductCard from "../../components/product-card/product-card.component";
+// import CategoryPreview from "../../components/category-preview/category-preview.component";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 
 import './shop.styles.scss';
 
 const Shop = () => {
-    const { categoriesMap } = useContext(CategoriesContext);
+    // const { categoriesMap } = useContext(CategoriesContext);
     return (
-        <div className="shop-container">
-            {Object.keys(categoriesMap).map((title) => {
-                const products = categoriesMap[title];
-                return (
-                    <CategoryPreview key={title} title={title} products={products} />
-                )
-            })}
-        </div>
+        <Routes>
+            <Route index element={<CategoriesPreview />} />
+            <Route path=":category" element={<Category />} />
+        </Routes>
+
+
+        // ***************OLD****************************
+        // <div className="shop-container">
+        //     {Object.keys(categoriesMap).map((title) => {
+        //         const products = categoriesMap[title];
+        //         return (
+        //             <CategoryPreview key={title} title={title} products={products} />
+        //         )
+        //     })}
+        // </div>
     )
 }
 
