e20c8e841aa5e2bc1bf03f105de936649105a44c
Kevin Binu Thottumkal <kevinthottumkal11@gmail.com>
2024-03-04 14:21:05 -0500

    set-up categories redux store

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
 import { UserReducer } from "./user/user.reducer";
+import { categoriesReducer } from './categories/categories.reducer';
 
 export const rootReducer = combineReducers({
     user: UserReducer,
+    categories: categoriesReducer,
 });
