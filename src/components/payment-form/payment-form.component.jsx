69153d96811578b8e3d7fc78ead47df287961ee7
Kevin Binu Thottumkal <kevinthottumkal11@gmail.com>
2024-03-11 16:45:11 -0400

    updated netlify functions

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -15,12 +15,40 @@ const PaymentForm = () => {
             return;
         }
 
-        
-    }
+        const response = await fetch("/.netlify/functions/create-payment-intent", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ amount: 1000 }),
+        }).then((res) => res.json());
+
+        const { paymentIntent: { client_secret } } = response;
+
+        console.log(client_secret);
+
+        const paymentResult = await stripe.confirmCardPayment(client_secret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: "Kevin Binu"
+                }
+            }
+        })
+
+        if (paymentResult.error) {
+            alert(paymentResult.error);
+        } else {
+            if (paymentResult.paymentIntent.status === 'succeeded') {
+                alert('Payment Successful');
+            }
+        }
+    };
+
     return (
         <PaymentFormContainer>
             <h2>Credit Card Payment: </h2>
-            <FormContainer>
+            <FormContainer onSubmit={ paymentHandler }>
                 <CardElement />
                 <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
             </FormContainer>
